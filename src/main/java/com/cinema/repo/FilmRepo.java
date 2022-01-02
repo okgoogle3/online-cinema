0c0240e17cfb1c386c57de252552636335e72b9c
okgoogle3 <46711269+okgoogle3@users.noreply.github.com>
2022-01-02 22:43:38 +0200

    Delete src directory

--- src/main/java/com/cinema/repo/FilmRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cinema.repo;
-
-import com.cinema.repo.model.Film;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FilmRepo extends JpaRepository<Film, Long> {
-}
